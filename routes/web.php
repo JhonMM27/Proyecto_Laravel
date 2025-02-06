32846b78c70674b520f5145c7f32be376c57e0e0
JhonMM <mesonesjhon27@gmail.com>
2025-02-05 23:31:25 -0500

    Primer parte del proyecto

---
+++ routes/web.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/inicio', function () {
+    return view('inicio');
+});
+
+Route::get('/productos',function(){
+    return view('almacen.producto');
+});
+
+
+Route::get('/usuario',function(){
+    $nombre = "Juan";
+    return "hola {$nombre}";
+});
+
+Route::get('/noticia/{titulo}',function($titulo){
+    return "Esta es la Noticia {$titulo}";
+});
+
+Route::group(['prefix'=>'admin'],function(){
+    Route::get('noticia',function(){
+        return "noticia";
+    });
+    Route::get('evento',function(){
+        return "evento";
+    });
+    Route::get('lavoral',function(){
+        return "lavoral"; 
+    });
+});
+
+Route::get('database', function(){
+    return env('DB_CONNECTION');
+});
+
+Route::get('time', function(){
+    return env('APP_TIMEZONE');
+});
+
+Route::get('local', function(){
+    return config('app.locale');
+});
+
+
+
+
+
